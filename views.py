eed1e284581ea801bf294f604234cd953a164503
Katerina <Katerina@MacBook-Pro.local>
2021-04-25 16:13:40 +0300

    create a file views.py that store routs for our website that are not related to authentication

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+# This file stores standart routs for our website that are not related to authentication 
 views = Blueprint('views', __name__)
 
 # going to the main page of our website
