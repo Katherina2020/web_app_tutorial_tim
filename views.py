f7776807c1ec539732d9027dfc2251cd61820bf1
Katerina <Katerina@MacBook-Pro.local>
2021-04-25 15:16:07 +0300

    create views blueprint

---
+++ views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+# going to the main page of our website
+@views.route('/')
+def home():
+    return render_template("home.html")
+    #return "<p>Hello</p>"
