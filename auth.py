013d76f72322c753100a4474337d81d8107d384c
Katerina <Katerina@MacBook-Pro.local>
2021-05-07 23:41:12 +0300

    auth.py get all the information from forms and make basic pythons checks to make sure that this information is valid

--- auth.py
+++ auth.py
@@ -4,8 +4,6 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data= request.form
-    print(data)
     return render_template("login.html", boolean = False,text="Testing", user="Kathy")
 
 @auth.route('/logout')
@@ -14,4 +12,22 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('first name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # if this information is not valid
+        # I don't want to create the new user account
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+            # add user to the database
     return render_template("sign_up.html")
