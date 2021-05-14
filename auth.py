e62dc37f61f7587910d06a5248f83ccc9798da0f
Katerina <Katerina@MacBook-Pro.local>
2021-05-14 18:05:03 +0300

    auth.py print info to terminal and flash messages

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -12,24 +12,26 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    data = request.form
+    print(data)
     # get all of the information from my forms
+    
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('first name')
+        print("email", email)
+        firstName = request.form.get('firstName')
+        print("first name", firstName)
         password1 = request.form.get('password1')
+        print("password1", password1)
         password2 = request.form.get('password2')
+        print("password2", password2)
 
-        # and made basic python checks if this information is valid
-        # if this information is not valid
-        # I don't want to create the new user account
-        if len(email) < 4:
-            pass
+        if len(email) < 10:
+            print("Email must be greater that 4 characters")
+            flash('Email must be greater than 4 characters', category='error')
         elif len(firstName) < 2:
             pass
-        elif password1 != password2:
-            pass
-        elif len(password1) < 7:
-            pass
         else:
-            # add user to the database
+            print("Account created!")
     return render_template("sign_up.html")
+        
