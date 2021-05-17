7fd831b6637620bd1befe056ad731e00dbda80a8
Katerina <Katerina@MacBook-Pro.Dlink>
2021-05-17 23:18:10 +0300

    __init__.py from database models import User note, add new function create_database, make a call of create _database function in create_app function

--- __init__.py
+++ __init__.py
@@ -1,10 +1,20 @@
 # this file would do website folder a python package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME ="database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hfyfik gfkukf'
-
+    # store the database in this website folder
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # initialize database
+    db.init_app(app)
+   
+    
     # register blueprint into init.py
     # we have a blueprint that is containing some 
     # some different views of a application
@@ -15,4 +25,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
