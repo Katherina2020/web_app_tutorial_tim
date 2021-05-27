2e2742e6aec5f39ed9e46766f427bf759c6e7612
Katerina <Katerina@MacBook-Pro.local>
2021-05-27 15:12:08 +0300

    add login_required decoration to home root so you can't go to the home page if you are not logged in

--- views.py
+++ views.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
 # This file stores standart routs for our website that are not related to authentication 
+from flask import Blueprint, render_template
+from flask_login import login_required,current_user
+
 views = Blueprint('views', __name__)
 
 # going to the main page of our website
 @views.route('/')
+@login_required     # now you can't go to the home page if you are not logged in
 def home():
     return render_template("home.html")
     
